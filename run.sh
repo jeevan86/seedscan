a4f7282d2bd7faf96e7b579baf418bb41314618c
Mike Hagerty <mth@Mikes-MacBook-Pro.local>
2014-01-14 13:38:59 -0500

    Modified build.xml test classpath to include ./src in order for java to find log4j.properties

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+# There are several ways to run seedscan:
+#
+# 0. >ant - default behavior is to compile the source files
+#           into the ./build class file dir
+#
+# 1. Using ant + build.xml to set the classpath and run seedscan from
+#    the ./build class files:
+#    >ant test
+#
 java -Xms512M -Xmx512M -jar SeedScan.jar
