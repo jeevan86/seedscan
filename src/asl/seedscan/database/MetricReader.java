7338de5c59dd0371d924c014818fc983d29ca33f
James Holland <jholland@usgs.gov>
2014-05-21 16:07:39 -0600

    Added a catch for ClassCastExceptions when casting results from tasks.

--- src/asl/seedscan/database/MetricReader.java
+++ src/asl/seedscan/database/MetricReader.java
@@ -4,23 +4,22 @@
 package asl.seedscan.database;
 
 import java.nio.ByteBuffer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import asl.concurrent.Task;
 import asl.concurrent.TaskThread;
-import asl.seedscan.metrics.MetricResult;
 
 /**
  * @author Joel D. Edwards <jdedwards@usgs.gov>
- *
+ * 
  */
 @SuppressWarnings("cast")
-public class MetricReader
-extends TaskThread<QueryContext<? extends Object>>
-{
-    private static final Logger logger = LoggerFactory.getLogger(asl.seedscan.database.MetricInjector.class);
-    
+public class MetricReader extends TaskThread<QueryContext<? extends Object>> {
+	private static final Logger logger = LoggerFactory
+			.getLogger(asl.seedscan.database.MetricInjector.class);
+
 	MetricDatabase metricDB;
 
 	/**
@@ -31,10 +30,11 @@ extends TaskThread<QueryContext<? extends Object>>
 		this.metricDB = metricDB;
 	}
 
-    public boolean isConnected() {
-        //System.out.println("== MetricReader.isConnected() = " + metricDB.isConnected() );
-        return metricDB.isConnected();
-    }
+	public boolean isConnected() {
+		// System.out.println("== MetricReader.isConnected() = " +
+		// metricDB.isConnected() );
+		return metricDB.isConnected();
+	}
 
 	/**
 	 * @param capacity
@@ -44,7 +44,9 @@ extends TaskThread<QueryContext<? extends Object>>
 		this.metricDB = metricDB;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see asl.concurrent.TaskThread#setup()
 	 */
 	@Override
@@ -52,43 +54,66 @@ extends TaskThread<QueryContext<? extends Object>>
 		// Pre-run logic goes here
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see asl.concurrent.TaskThread#performTask(asl.concurrent.Task)
 	 */
+	/**
+	 * Perform task can generate an ClassCastException if a result is
+	 * unexpected. This will be caught and logged. The Task will fail however
+	 * and may generate some null pointers.
+	 */
+	@SuppressWarnings({ "unchecked" })
 	@Override
 	protected void performTask(Task<QueryContext<? extends Object>> task) {
 		String command = task.getCommand();
-		
+
 		logger.info("performTask: command=" + command + " task=" + task);
-		
+
 		try {
 			if (command.equals("GET-METRIC-DIGEST")) {
-				MetricContext<ByteBuffer> context = (MetricContext<ByteBuffer>)task.getData(); 
+				MetricContext<ByteBuffer> context = (MetricContext<ByteBuffer>) task
+						.getData();
 				MetricValueIdentifier id = context.getId();
-				ByteBuffer digest = metricDB.getMetricDigest(id.getDate(), id.getMetricName(), id.getStation());
-				QueryResult<ByteBuffer> result = new QueryResult<ByteBuffer>(digest);
+				ByteBuffer digest = metricDB.getMetricDigest(id.getDate(),
+						id.getMetricName(), id.getStation());
+				QueryResult<ByteBuffer> result = new QueryResult<ByteBuffer>(
+						digest);
 				context.getReplyQueue().put(result);
-			}
-			else if (command.equals("GET-METRIC-VALUE-DIGEST")) {
-				MetricContext<ByteBuffer> context = (MetricContext<ByteBuffer>)task.getData(); 
+			} else if (command.equals("GET-METRIC-VALUE-DIGEST")) {
+				MetricContext<ByteBuffer> context = (MetricContext<ByteBuffer>) task
+						.getData();
 				MetricValueIdentifier id = context.getId();
-				ByteBuffer digest = metricDB.getMetricValueDigest(id.getDate(), id.getMetricName(), id.getStation(), id.getChannel());
-				QueryResult<ByteBuffer> result = new QueryResult<ByteBuffer>(digest);
+				ByteBuffer digest = metricDB.getMetricValueDigest(id.getDate(),
+						id.getMetricName(), id.getStation(), id.getChannel());
+				QueryResult<ByteBuffer> result = new QueryResult<ByteBuffer>(
+						digest);
 				context.getReplyQueue().put(result);
-			}
-			else if (command.equals("GET-METRIC-VALUE")) {
-				MetricContext<Double> context = (MetricContext<Double>)task.getData(); 
+			} else if (command.equals("GET-METRIC-VALUE")) {
+				MetricContext<Double> context = (MetricContext<Double>) task
+						.getData();
 				MetricValueIdentifier id = context.getId();
-				Double value = metricDB.getMetricValue(id.getDate(), id.getMetricName(), id.getStation(), id.getChannel());
+				Double value = metricDB.getMetricValue(id.getDate(),
+						id.getMetricName(), id.getStation(), id.getChannel());
 				QueryResult<Double> result = new QueryResult<Double>(value);
 				context.getReplyQueue().put(result);
 			}
+		} catch (ClassCastException ex) {
+			logger.error(
+					"A cast in MetricReader, performTask failed due to unchecked type.",
+					ex);
 		} catch (InterruptedException ex) {
-			logger.warn("Interrupted while attempting to send reply. This may have caused a station thread to hang!", ex);
+			logger.warn(
+					"Interrupted while attempting to send reply. This may have caused a station thread to hang!",
+					ex);
 		}
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see asl.concurrent.TaskThread#cleanup()
 	 */
 	@Override
@@ -96,44 +121,50 @@ extends TaskThread<QueryContext<? extends Object>>
 		// Post-run logic goes here
 	}
 
-	public Double getMetricValue(MetricValueIdentifier id)
-	{
+	public Double getMetricValue(MetricValueIdentifier id) {
 		Double value = null;
 		try {
 			MetricContext<Double> context = new MetricContext<Double>(id);
 			addTask("GET-METRIC-VALUE", context);
 			value = context.getReplyQueue().take().getResult();
 		} catch (InterruptedException ex) {
-			logger.warn("Interrupted while awaiting reply from database reader thread.", ex);
+			logger.warn(
+					"Interrupted while awaiting reply from database reader thread.",
+					ex);
 		}
 		return value;
 	}
-	
-	public ByteBuffer getMetricDigest(MetricValueIdentifier id)
-	{
+
+	public ByteBuffer getMetricDigest(MetricValueIdentifier id) {
 		ByteBuffer digest = null;
 		try {
-			MetricContext<ByteBuffer> context = new MetricContext<ByteBuffer>(id);
+			MetricContext<ByteBuffer> context = new MetricContext<ByteBuffer>(
+					id);
 			addTask("GET-METRIC-DIGEST", context);
 			digest = context.getReplyQueue().take().getResult();
 		} catch (InterruptedException ex) {
-			logger.warn("Interrupted while awaiting reply from database reader thread.", ex);
+			logger.warn(
+					"Interrupted while awaiting reply from database reader thread.",
+					ex);
 		}
 		return digest;
 	}
-	
-    /**
-     * Currently getMetricValueDigest() is the only method called (from the MetricData class)
-    */
-	public ByteBuffer getMetricValueDigest(MetricValueIdentifier id)
-	{
+
+	/**
+	 * Currently getMetricValueDigest() is the only method called (from the
+	 * MetricData class)
+	 */
+	public ByteBuffer getMetricValueDigest(MetricValueIdentifier id) {
 		ByteBuffer digest = null;
 		try {
-			MetricContext<ByteBuffer> context = new MetricContext<ByteBuffer>(id);
+			MetricContext<ByteBuffer> context = new MetricContext<ByteBuffer>(
+					id);
 			addTask("GET-METRIC-VALUE-DIGEST", context);
 			digest = context.getReplyQueue().take().getResult();
 		} catch (InterruptedException ex) {
-			logger.warn("Interrupted while awaiting reply from database reader thread.", ex);
+			logger.warn(
+					"Interrupted while awaiting reply from database reader thread.",
+					ex);
 		}
 		return digest;
 	}
