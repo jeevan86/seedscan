8a8aec587d5652f75ded0cda94e0f4db1ac32b2d
James Holland <jholland@usgs.gov>
2014-05-21 13:52:44 -0600

    Cleaning up MetricResult source.

--- src/asl/seedscan/metrics/MetricResult.java
+++ src/asl/seedscan/metrics/MetricResult.java
@@ -32,113 +32,100 @@ import java.util.TreeSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MetricResult
-{
-    private static final Logger logger = LoggerFactory.getLogger(asl.seedscan.metrics.MetricResult.class);
+public class MetricResult {
+	private static final Logger logger = LoggerFactory
+			.getLogger(asl.seedscan.metrics.MetricResult.class);
 
-    private String metricName;
-    private Calendar date;
-    private Station station;
-    private Hashtable<String, Double> valueMap;
-    private Hashtable<String, ByteBuffer> digestMap;
+	private String metricName;
+	private Calendar date;
+	private Station station;
+	private Hashtable<String, Double> valueMap;
+	private Hashtable<String, ByteBuffer> digestMap;
 
-    public MetricResult(StationMeta stationInfo, String metricName)
-    {
-    	this.metricName = metricName;
-    	this.date = (Calendar)stationInfo.getTimestamp().clone();
-    	this.station = new Station(stationInfo.getNetwork(), stationInfo.getStation());
-        this.valueMap = new Hashtable<String, Double>();
-        this.digestMap = new Hashtable<String, ByteBuffer>();
-    }
-    
-    public String getMetricName()
-    {
-    	return metricName;
-    }
-    
-    public Calendar getDate()
-    {
-    	return date;
-    }
-    
-    public Station getStation()
-    {
-    	return station;
-    }
+	public MetricResult(StationMeta stationInfo, String metricName) {
+		this.metricName = metricName;
+		this.date = (Calendar) stationInfo.getTimestamp().clone();
+		this.station = new Station(stationInfo.getNetwork(),
+				stationInfo.getStation());
+		this.valueMap = new Hashtable<String, Double>();
+		this.digestMap = new Hashtable<String, ByteBuffer>();
+	}
 
-    public void addResult(Channel channel, Double value, ByteBuffer digest)
-    {
-        addResult(createResultId(channel), value, digest);
-    }
-    
-    public void addResult(Channel channelA, Channel channelB, Double value, ByteBuffer digest)
-    {
-        addResult(createResultId(channelA, channelB), value, digest);
-    }
-    
-    public void addResult(String id, Double value, ByteBuffer digest)
-    {
-        valueMap.put(id, value);
-        digestMap.put(id, digest);
-    }
+	public String getMetricName() {
+		return metricName;
+	}
 
-    public Double getResult(String id)
-    {
-        return valueMap.get(id);
-    }
-    
-    public ByteBuffer getDigest(String id)
-    {
-    	return digestMap.get(id);
-    }
+	public Calendar getDate() {
+		return date;
+	}
 
-    public Enumeration<String> getIds()
-    {
-        return valueMap.keys();
-    }
+	public Station getStation() {
+		return station;
+	}
 
-    public Set<String> getIdSet()
-    {
-        return valueMap.keySet();
-    }
+	public void addResult(Channel channel, Double value, ByteBuffer digest) {
+		addResult(createResultId(channel), value, digest);
+	}
 
-    public SortedSet<String> getIdSortedSet()
-    {
-        return new TreeSet<String>(valueMap.keySet());
-    }
+	public void addResult(Channel channelA, Channel channelB, Double value,
+			ByteBuffer digest) {
+		addResult(createResultId(channelA, channelB), value, digest);
+	}
 
- // Static methods
-    public static String createResultId(Channel channel)
-    {
-    	return String.format("%s,%s", channel.getLocation(), channel.getChannel());
-    }
-    
-    public static String createResultId(Channel channelA, Channel channelB)
-    {
-    	return String.format("%s-%s,%s-%s", channelA.getLocation(), channelB.getLocation(),
-    										channelA.getChannel(),  channelB.getChannel());
-    }
+	public void addResult(String id, Double value, ByteBuffer digest) {
+		valueMap.put(id, value);
+		digestMap.put(id, digest);
+	}
 
-    public static Channel createChannel(String id)
-    {
-    	logger.info("createChannel: id=" + id);
-    	Channel channel = null;
-        if (id.length() > 15) { // This is likely to be coming from a CalibrationMetric
-                              // so handle differently
-            // id = "{ "channelId":"00-LHZ", "band": { ..."
-            String[] f1 = id.split(":");       // f1="00-LHZ","band"
-            String[] f2 = f1[1].split(",");    // f2="00-LHZ"
-            String[] f3 = f2[0].split("-");    // f3[0]="00" f3[1]="LHZ"
-            channel = new Channel(f3[0],f3[1]);
-        }
-        else {
-    	    String[] parts = id.split(",");
-    	    if (parts.length == 2) {
-    		    channel = new Channel(parts[0], parts[1]);
-    	    }
-        }
-        logger.info("createChannel: channel=[{}]", channel);
-    	return channel;
-    }
+	public Double getResult(String id) {
+		return valueMap.get(id);
+	}
+
+	public ByteBuffer getDigest(String id) {
+		return digestMap.get(id);
+	}
+
+	public Enumeration<String> getIds() {
+		return valueMap.keys();
+	}
+
+	public Set<String> getIdSet() {
+		return valueMap.keySet();
+	}
+
+	public SortedSet<String> getIdSortedSet() {
+		return new TreeSet<String>(valueMap.keySet());
+	}
+
+	// Static methods
+	public static String createResultId(Channel channel) {
+		return String.format("%s,%s", channel.getLocation(),
+				channel.getChannel());
+	}
+
+	public static String createResultId(Channel channelA, Channel channelB) {
+		return String.format("%s-%s,%s-%s", channelA.getLocation(),
+				channelB.getLocation(), channelA.getChannel(),
+				channelB.getChannel());
+	}
+
+	public static Channel createChannel(String id) {
+		logger.info("createChannel: id=" + id);
+		Channel channel = null;
+		if (id.length() > 15) { // This is likely to be coming from a
+			// CalibrationMetric so handle differently
+			// id = "{ "channelId":"00-LHZ", "band": { ..."
+			String[] f1 = id.split(":"); // f1="00-LHZ","band"
+			String[] f2 = f1[1].split(","); // f2="00-LHZ"
+			String[] f3 = f2[0].split("-"); // f3[0]="00" f3[1]="LHZ"
+			channel = new Channel(f3[0], f3[1]);
+		} else {
+			String[] parts = id.split(",");
+			if (parts.length == 2) {
+				channel = new Channel(parts[0], parts[1]);
+			}
+		}
+		logger.info("createChannel: channel=[{}]", channel);
+		return channel;
+	}
 }
-
