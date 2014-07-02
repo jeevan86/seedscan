ef4fa8a10a52d44a8b0de1c4202f856a2ccdb67b
James Holland <jholland@usgs.gov>
2014-07-01 19:15:02 -0600

    Added javadoc.xml and basic javadoc comments to Seedscan.java and metric.java

--- src/asl/seedscan/SeedScan.java
+++ src/asl/seedscan/SeedScan.java
@@ -1,21 +1,3 @@
-/*
- * Copyright 2011, United States Geological Survey or
- * third-party contributors as indicated by the @author tags.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/  >.
- *
- */
 package asl.seedscan;
 
 import java.io.File;
@@ -48,12 +30,23 @@ import asl.seedscan.metrics.MetricWrapper;
 import asl.util.Filter;
 
 /**
- * 
+ * The Class SeedScan.
+ * @author James Holland - USGS
+ * @author Joel Edwards - USGS
+ * @author Mike Hagerty
+ * @author Alejandro Gonzales - Honeywell
  */
 public class SeedScan {
+	
+	/** The logger. */
 	private static Logger logger = LoggerFactory
 			.getLogger(asl.seedscan.SeedScan.class);
 
+	/**
+	 * The main method for seedscan.
+	 *
+	 * @param args command line arguments. 
+	 */
 	public static void main(String args[]) {
 		// Default locations of config and schema files
 		File configFile = new File("config.xml");
--- src/asl/seedscan/metrics/Metric.java
+++ src/asl/seedscan/metrics/Metric.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2012, United States Geological Survey or
- * third-party contributors as indicated by the @author tags.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/  >.
- *
- */
+
 package asl.seedscan.metrics;
 
 import java.util.Calendar;
@@ -37,26 +20,54 @@ import asl.seedscan.database.MetricValueIdentifier;
 import asl.seedscan.event.EventCMT;
 import freq.Cmplx;
 
+/**
+ * The basic class that all metrics extend.
+ * @author Mike Hagerty
+ * @author Joel Edwards - USGS
+ * @author James Holland - USGS
+ * @author Alejandro Gonzales - Honeywell
+ */
 public abstract class Metric {
+	
 	private static final Logger logger = LoggerFactory
 			.getLogger(asl.seedscan.metrics.Metric.class);
 
+	/** The arguments. */
 	private Hashtable<String, String> arguments;
+	
+	/** The cross power map. */
 	private Hashtable<CrossPowerKey, CrossPower> crossPowerMap;
 
+	/** Determines if metric will be forced to recompute and update the database.*/
 	private boolean forceUpdate = false;
+	
+	/** Determines if plots will be created. */
 	private boolean makePlots = false;
+	
+	/** The output directory for plots. */
 	private String outputDir = null;
 
+	/** Dummy value when no results is computed. */
 	protected final double NO_RESULT = -999.999;
 
+	/** The station metadata. */
 	protected StationMeta stationMeta = null;
+	
+	/** The metric data. */
 	protected MetricData metricData = null;
+	
+	/** The metric result. */
 	protected MetricResult metricResult = null;
 
+	/** The event table. */
 	private Hashtable<String, EventCMT> eventTable = null;
+	
+	/** The event synthetics. */
 	private Hashtable<String, Hashtable<String, SacTimeSeries>> eventSynthetics = null;
 
+	/**
+	 * Instantiates a new metric.
+	 */
 	public Metric() {
 		arguments = new Hashtable<String, String>();
 		crossPowerMap = new Hashtable<CrossPowerKey, CrossPower>();
@@ -67,39 +78,87 @@ public abstract class Metric {
 		addArgument("forceupdate");
 	}
 
+	/**
+	 * Sets the data.
+	 *
+	 * @param metricData the new data
+	 */
 	public void setData(MetricData metricData) {
 		this.metricData = metricData;
 		stationMeta = metricData.getMetaData();
 		metricResult = new MetricResult(stationMeta, getName());
 	}
 
+	/**
+	 * Gets the version.
+	 *
+	 * @return the version
+	 */
 	public abstract long getVersion();
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the metric name
+	 */
 	public abstract String getName();
 
+	/**
+	 * Process.
+	 */
 	public abstract void process();
 
+	/**
+	 * Creates the identifier.
+	 *
+	 * @param channel the channel
+	 * @return the metric value identifier
+	 */
 	public MetricValueIdentifier createIdentifier(Channel channel) {
 		return new MetricValueIdentifier(metricResult.getDate(),
 				metricResult.getMetricName(), metricResult.getStation(),
 				channel);
 	}
 
+	/**
+	 * Creates the identifier for multiple channels.
+	 *
+	 * @param channelA
+	 * @param channelB
+	 * @return the metric value identifier
+	 */
 	public MetricValueIdentifier createIdentifier(Channel channelA,
 			Channel channelB) {
 		return createIdentifier(MetricResult.createChannel(MetricResult
 				.createResultId(channelA, channelB)));
 	}
 
+	/**
+	 * Gets the cross power map.
+	 *
+	 * @return the cross power map
+	 */
 	public Hashtable<CrossPowerKey, CrossPower> getCrossPowerMap() {
 		return crossPowerMap;
 	}
 
+	/**
+	 * Sets the cross power map.
+	 *
+	 * @param crossPowerMap the cross power map
+	 */
 	public void setCrossPowerMap(
 			Hashtable<CrossPowerKey, CrossPower> crossPowerMap) {
 		this.crossPowerMap = crossPowerMap;
 	}
 
+	/**
+	 * Gets the cross power.
+	 *
+	 * @param channelA
+	 * @param channelB
+	 * @return the cross power
+	 */
 	protected CrossPower getCrossPower(Channel channelA, Channel channelB) {
 		CrossPowerKey key = new CrossPowerKey(channelA, channelB);
 		CrossPower crossPower;
@@ -125,6 +184,12 @@ public abstract class Metric {
 		return crossPower;
 	}
 
+	/**
+	 * Gets the event synthetics.
+	 *
+	 * @param eventIdString the event id string
+	 * @return the event synthetics
+	 */
 	public Hashtable<String, SacTimeSeries> getEventSynthetics(
 			String eventIdString) {
 		if (eventSynthetics.containsKey(eventIdString)) {
@@ -137,31 +202,66 @@ public abstract class Metric {
 		}
 	}
 
+	/**
+	 * Gets the event table.
+	 *
+	 * @return the event table
+	 */
 	public Hashtable<String, EventCMT> getEventTable() {
 		return eventTable;
 	}
 
+	/**
+	 * Sets the event synthetics.
+	 *
+	 * @param eventSynthetics the event synthetics
+	 */
 	public void setEventSynthetics(
 			Hashtable<String, Hashtable<String, SacTimeSeries>> eventSynthetics) {
 		this.eventSynthetics = eventSynthetics;
 	}
 
+	/**
+	 * Sets the event table.
+	 *
+	 * @param events the events
+	 */
 	public void setEventTable(Hashtable<String, EventCMT> events) {
 		eventTable = events;
 	}
 
+	/**
+	 * Gets the metric result.
+	 *
+	 * @return the metric result
+	 */
 	public MetricResult getMetricResult() {
 		return metricResult;
 	}
 
+	/**
+	 * Sets the base output dir.
+	 *
+	 * @param outputDir the new base output dir
+	 */
 	public void setBaseOutputDir(String outputDir) {
 		this.outputDir = outputDir;
 	}
 
+	/**
+	 * Gets the base output dir.
+	 *
+	 * @return the outputDir private field.
+	 */
 	public String getBaseOutputDir() {
 		return outputDir;
 	}
 
+	/**
+	 * Gets the output dir.
+	 *
+	 * @return the output dir structure with dates. e.g., "outputs/2012/2012160/2012160.IU_ANMO"
+	 */
 	public String getOutputDir() {
 		// e.g., "outputs/2012/2012160/2012160.IU_ANMO"
 		return new String(String.format("%s/%4s/%4s%3s/%4s%3s.%s",
@@ -169,51 +269,104 @@ public abstract class Metric {
 				getDOY(), getStation()));
 	}
 
+	/**
+	 * Gets the day.
+	 *
+	 * @return the date string of stationMeta's timestamp.
+	 */
 	public String getDay() { // returns yyyy:ddd:hh:mm
 		return (EpochData.epochToDateString(stationMeta.getTimestamp()));
 	}
 
+	/**
+	 * Gets the date.
+	 *
+	 * @return the stationMeta timestamp.
+	 */
 	public Calendar getDate() {
 		// returns Calendar date from StationMeta
 		return stationMeta.getTimestamp();
 	}
 
+	/**
+	 * Gets the day of year.
+	 *
+	 * @return the day of year
+	 */
 	public String getDOY() {
 		String[] dateArray = getDay().split(":");
 		return dateArray[1];
 	}
 
+	/**
+	 * Gets the year.
+	 *
+	 * @return the year
+	 */
 	public String getYear() {
 		String[] dateArray = getDay().split(":");
 		return dateArray[0];
 	}
 
+	/**
+	 * Gets the station name.
+	 *
+	 * @return the station name e.g., will return "ANMO"
+	 */
 	public String getStn() // e.g., will return "ANMO"
 	{
 		return stationMeta.getStation();
 	}
 
+	/**
+	 * Gets the network and station name.
+	 *
+	 * @return the network and station name e.g., will return "IU_ANMO"
+	 */
 	public String getStation() // e.g., will return "IU_ANMO"
 	{
 		return metricResult.getStation().toString();
 	}
 
+	/**
+	 * Sets the force update to true.
+	 */
 	private void setForceUpdate() {
 		this.forceUpdate = true;
 	}
 
+	/**
+	 * Gets the force update.
+	 *
+	 * @return the force update field
+	 */
 	public boolean getForceUpdate() {
 		return forceUpdate;
 	}
 
+	/**
+	 * Sets the make plots to true.
+	 */
 	private void setMakePlots() {
 		this.makePlots = true;
 	}
 
+	/**
+	 * Gets the make plots.
+	 *
+	 * @return the make plots field
+	 */
 	public boolean getMakePlots() {
 		return makePlots;
 	}
 
+	/**
+	 * Check if both station metadata and metric data have the channels and power bands.
+	 *
+	 * @param location the location
+	 * @param band the power band
+	 * @return true, if both stationMeta and metric data have channels and band.
+	 */
 	public boolean weHaveChannels(String location, String band) {
 		if (!stationMeta.hasChannels(location, band)) {
 			return false;
@@ -224,11 +377,23 @@ public abstract class Metric {
 		return true;
 	}
 
-	// Dynamic argument managment
+	/**
+	 * Dynamic argument managment
+	 * Adds the argument to the arguments hashtable with empty string as value.
+	 *
+	 * @param name the name of the argument being added to the hashtable.
+	 */
 	protected final void addArgument(String name) {
 		arguments.put(name, "");
 	}
 
+	/**
+	 * Sets the value of the argument in the arguments hashtable.
+	 *
+	 * @param name argument name/key
+	 * @param value argument's new value
+	 * @throws NoSuchFieldException the field was never added to arguments.
+	 */
 	public final void add(String name, String value)
 			throws NoSuchFieldException {
 		if (!arguments.containsKey(name)) {
@@ -255,7 +420,11 @@ public abstract class Metric {
 	/**
 	 * get(key) returns value if key is found in Hashtable arguments returns
 	 * null if key is found but value is not set (value="") throws
-	 * NoSuchFieldException if key is not found
+	 * NoSuchFieldException if key is not found.
+	 *
+	 * @param name the argument's name/key
+	 * @return the value of the hashtable
+	 * @throws NoSuchFieldException the field was never added to arguments.
 	 */
 	public final String get(String name) throws NoSuchFieldException {
 		if (!arguments.containsKey(name)) {
@@ -271,6 +440,11 @@ public abstract class Metric {
 		return argumentValue;
 	}
 
+	/**
+	 * Returns list of keys in arguments hashtable.
+	 *
+	 * @return the list of keys in arguments
+	 */
 	public final Enumeration<String> names() {
 		return arguments.keys();
 	}
@@ -281,16 +455,13 @@ public abstract class Metric {
 	 * 
 	 * Use Peterson's algorithm (24 hrs = 13 segments with 75% overlap, etc.)
 	 * 
-	 * @param channelX
-	 *            - X-channel used for power-spectral-density computation
-	 * @param channelY
-	 *            - Y-channel used for power-spectral-density computation
-	 * @param params
-	 *            [] - Dummy array used to pass df (frequency spacing) back up
-	 * 
+	 * @param channelX            - X-channel used for power-spectral-density computation
+	 * @param channelY            - Y-channel used for power-spectral-density computation
+	 * @param params            [] - Dummy array used to pass df (frequency spacing) back up
 	 * @return psd[f] - Contains smoothed crosspower-spectral density computed
 	 *         for nf = nfft/2 + 1 frequencies (+ve freqs + DC + Nyq)
-	 * @author Mike Hagerty
+	 * @throws ChannelMetaException the channel metadata exception
+	 * @throws MetricPSDException the metric psd exception
 	 */
 	private final double[] computePSD(Channel channelX, Channel channelY,
 			double[] params) throws ChannelMetaException, MetricPSDException {
