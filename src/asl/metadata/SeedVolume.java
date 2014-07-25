26814597cf82f95f91880675ddd3838991119b1f
James Holland <jholland@usgs.gov>
2014-07-24 20:58:15 -0600

    Added javadoc comments to SeedVolume

--- src/asl/metadata/SeedVolume.java
+++ src/asl/metadata/SeedVolume.java
@@ -8,22 +8,43 @@ import java.util.TreeSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Class SeedVolume.
+ * 
+ * @author Joel Edwards - USGS
+ * @author Mike Hagerty
+ */
 public class SeedVolume {
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(asl.metadata.SeedVolume.class);
 
+	/** The volume info blockette: blockette 10. */
 	private Blockette volumeInfo = null;
+
+	/** The network key. This is based on info blockette field 9: volume label */
 	private NetworkKey networkKey = null;
 
+	/** The station locators. */
 	private ArrayList<Blockette> stationLocators;
+
+	/** The stations. */
 	private Hashtable<StationKey, StationData> stations;
 
-	// constructor(s)
+	/**
+	 * Instantiates a new seed volume.
+	 */
 	public SeedVolume() {
 		stations = new Hashtable<StationKey, StationData>();
 		stationLocators = new ArrayList<Blockette>();
 	}
 
+	/**
+	 * Instantiates a new seed volume.
+	 * 
+	 * @param volumeInfo
+	 *            the volume info
+	 */
 	public SeedVolume(Blockette volumeInfo) {
 		this.volumeInfo = volumeInfo;
 		try {
@@ -35,41 +56,93 @@ public class SeedVolume {
 		stationLocators = new ArrayList<Blockette>();
 	}
 
-	// stations
+	/**
+	 * Adds a station to the station hashtable
+	 * 
+	 * @param key
+	 *            the station key
+	 * @param data
+	 *            the station metadata
+	 */
 	public void addStation(StationKey key, StationData data) {
 		stations.put(key, data);
 	}
 
+	/**
+	 * Checks for station in the station hashtable
+	 * 
+	 * @param key
+	 *            the key
+	 * @return true, if station hashtable has stationkey
+	 */
 	public boolean hasStation(StationKey key) {
 		return stations.containsKey(key);
 	}
 
+	/**
+	 * Gets the station metadata.
+	 * 
+	 * @param key
+	 *            the key
+	 * @return the station
+	 */
 	public StationData getStation(StationKey key) {
 		return stations.get(key);
 	}
 
 	// volume info
+	/**
+	 * Sets the volume info.
+	 * 
+	 * @param volumeInfo
+	 *            the new volume info
+	 */
 	public void setVolumeInfo(Blockette volumeInfo) {
 		this.volumeInfo = volumeInfo;
 	}
 
+	/**
+	 * Gets the volume info.
+	 * 
+	 * @return the volume info
+	 */
 	public Blockette getVolumeInfo() {
 		return this.volumeInfo;
 	}
 
+	/**
+	 * Gets the network key.
+	 * 
+	 * @return the network key
+	 */
 	public NetworkKey getNetworkKey() {
 		return this.networkKey;
 	}
 
-	// station locators (list of stations in seed volume)
+	/**
+	 * station locators (list of stations in seed volume)
+	 * 
+	 * @param stationLocator
+	 *            the station locator
+	 */
 	public void addStationLocator(Blockette stationLocator) {
 		stationLocators.add(stationLocator);
 	}
 
+	/**
+	 * Gets the station locators.
+	 * 
+	 * @return the station locators
+	 */
 	public ArrayList<Blockette> getStationLocators() {
 		return stationLocators;
 	}
 
+	/**
+	 * Prints the stations.
+	 * 
+	 * @deprecated It is no longer used.
+	 */
 	public void printStations() {
 		TreeSet<StationKey> keys = new TreeSet<StationKey>();
 		keys.addAll(stations.keySet());
@@ -78,6 +151,11 @@ public class SeedVolume {
 		}
 	}
 
+	/**
+	 * Gets the station list.
+	 * 
+	 * @return the station list
+	 */
 	public List<Station> getStationList() {
 		ArrayList<Station> stns = new ArrayList<Station>();
 		TreeSet<StationKey> keys = new TreeSet<StationKey>();
