ac5a532ca22cd733fcc4a74722ecdf6e3737e9bd
James Holland <jholland@usgs.gov>
2014-07-24 18:30:07 -0600

    Added comments and logger messages to show the determined network and if the volume label is not simply the network code.

--- src/asl/metadata/NetworkKey.java
+++ src/asl/metadata/NetworkKey.java
@@ -1,54 +1,78 @@
-/*
- * Copyright 2012, United States Geological Survey or
- * third-party contributors as indicated by the @author tags.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/  >.
- *
- */
 package asl.metadata;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Class NetworkKey. This is used to identify which volume contains station
+ * data in {@link asl.metadata.MetaGenerator#volumes}
+ * 
+ * @author Mike Hagerty
+ * @author James Holland
+ */
 public class NetworkKey extends Key {
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(asl.metadata.NetworkKey.class);
 
+	/**
+	 * The Constant DATALESS_VOLUME_BLOCKETTE_NUMBER. Volume info blockettes are
+	 * always blockette 10
+	 */
 	public static final int DATALESS_VOLUME_BLOCKETTE_NUMBER = 10;
 
+	/** The network code e.g. IU, CU, IC. */
 	private String network = null;
 
-	// constructor(s)
+	/**
+	 * Instantiates a new network key based on volume info blockette
+	 * 
+	 * @param blockette
+	 *            the volume info blockette, blockette 10
+	 * @throws WrongBlocketteException
+	 *             the blockette passed was not blockette 10
+	 */
 	public NetworkKey(Blockette blockette) throws WrongBlocketteException {
 		if (blockette.getNumber() != DATALESS_VOLUME_BLOCKETTE_NUMBER) {
 			throw new WrongBlocketteException();
 		}
-		//TODO: The assumption that blockette 10 field 9 will always be either XX or XX* is wrong.
-		//This field is optional and can vary widely.
-		//This varLabel.substring(0,2) is a hack to get it working after Iris switched to XX.dataless
+		// TODO: The assumption that blockette 10 field 9 will always be either
+		// XX or XX* is wrong.
+		// This field is optional and can vary widely.
+		// This varLabel.substring(0,2) is a hack to get it working after Iris
+		// switched to XX.dataless
 		String varLabel = blockette.getFieldValue(9, 0);
-		network = varLabel.substring(0,2);
+		network = varLabel.substring(0, 2);
+		logger.info("Network key created for [{}]", network);
+		if (network != varLabel) {
+			logger.warn(
+					"Volume label doesn't match determined network. Volume label = [{}]",
+					varLabel);
+		}
 	}
 
+	/**
+	 * Instantiates a new network key based on a string passed.
+	 * 
+	 * @param network
+	 *            the network code e.g. IU
+	 */
 	public NetworkKey(String network) {
 		this.network = network;
 	}
 
+	/**
+	 * Gets the network code
+	 * 
+	 * @return the network code
+	 */
 	public String getNetwork() {
 		return network;
 	}
 
+	/**
+	 * @see asl.metadata.Key#toString()
+	 */
 	public String toString() {
 		return network;
 	}
