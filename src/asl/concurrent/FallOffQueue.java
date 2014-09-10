51a4b4f979a50129ed1073aca40aa203fdd0ac77
James Holland <jholland@usgs.gov>
2014-09-10 14:43:15 -0600

    Removed unused addAll function from FallOffQueue. It was only "added because the parent had it".

--- src/asl/concurrent/FallOffQueue.java
+++ src/asl/concurrent/FallOffQueue.java
@@ -83,39 +83,6 @@ public class FallOffQueue<E> extends LinkedBlockingQueue<E> {
 		return result;
 	}
 
-	// Just because the parent class has it...
-	/**
-	 * Attempt to add an element to the queue, waiting until there is space
-	 * available. This is only here because the parent class has it. Sine we
-	 * simply push off any elements in our way, there should never be any need
-	 * to wait.
-	 * 
-	 * @param e
-	 *            The element being offered.
-	 * @param timeout
-	 *            The maximum amount of time to wait for the element to be
-	 *            added.
-	 * @param unit
-	 *            The units for the timeout argument.
-	 * @return True if the element was added, otherwise false.
-	 * @throws InterruptedException
-	 *             If this thread was interrupted while waiting to add the
-	 *             element.
-	 */
-	public boolean offer(E e, long timeout, TimeUnit unit)
-			throws InterruptedException {
-		boolean result = true;
-		if (m_capacity == 0) {
-			result = false;
-		} else {
-			if (remainingCapacity() == 0) {
-				poll();
-			}
-			result = super.offer(e, timeout, unit);
-		}
-		return result;
-	}
-
 	/**
 	 * Add an element to the queue.
 	 * 
