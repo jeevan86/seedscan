f73cd9b461053fd35407d5d8c6f19cdc3c6cff84
James Holland <jholland@usgs.gov>
2015-06-09 16:15:57 -0600

    Documented MetricException and tightened permissions.

--- src/asl/seedscan/metrics/MetricException.java
+++ src/asl/seedscan/metrics/MetricException.java
@@ -1,13 +1,29 @@
 package asl.seedscan.metrics;
 
-public class MetricException extends Exception {
+/**
+ * The Class MetricException.
+ * 
+ * This is a generic exception used when a metric needs an exception.
+ */
+class MetricException extends Exception {
+
+	/** The Constant serial version UID. */
 	private static final long serialVersionUID = 1L;
 
-	public MetricException() {
+	/**
+	 * Instantiates a new metric exception. This isn't used.
+	 */
+	MetricException() {
 		super();
 	}
 
-	public MetricException(String message) {
+	/**
+	 * Instantiates a new metric exception.
+	 * 
+	 * @param message
+	 *            the exception message.
+	 */
+	MetricException(String message) {
 		super(message);
 	}
 }
