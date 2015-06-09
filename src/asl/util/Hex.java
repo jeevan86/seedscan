8fd57b5317cfef2c3081e3c63b0d6e95e8ae234a
James Holland <jholland@usgs.gov>
2015-06-09 16:46:35 -0600

    Removed unused code in Hex class. I plan to completely remove this class as there is no reason to convert the digest's bytebuffer to hex string. It appears it isn't even used anyways.

--- src/asl/util/Hex.java
+++ src/asl/util/Hex.java
@@ -1,7 +1,5 @@
 package asl.util;
 
-import java.nio.ByteBuffer;
-
 /**
  * 
  * @author Joel D. Edwards - USGS
@@ -18,16 +16,9 @@ public class Hex {
 		}
 	}
 
-	public static String byteBufferToHexString(ByteBuffer byteBuffer,
-			boolean upperCase) throws IllegalArgumentException {
-		try {
-			return byteArrayToHexString(byteBuffer.array());
-		} catch (IllegalArgumentException e) {
-			throw e;
-		}
-	}
+	
 
-	public static String byteArrayToHexString(byte[] byteArray,
+	private static String byteArrayToHexString(byte[] byteArray,
 			boolean upperCase) throws IllegalArgumentException {
 		StringBuilder builder = new StringBuilder();
 		for (byte b : byteArray) {
@@ -43,39 +34,11 @@ public class Hex {
 		return builder.toString();
 	}
 
-	public static ByteBuffer hexStringToByteBuffer(String hexString)
-			throws IllegalArgumentException {
-		try {
-			return ByteBuffer.wrap(hexStringToByteArray(hexString));
-		} catch (IllegalArgumentException e) {
-			throw e;
-		}
-	}
+	
 
-	public static byte[] hexStringToByteArray(String hexString)
-			throws IllegalArgumentException {
-		char[] chars = hexString.toCharArray();
-		int byteCount = chars.length / 2 + chars.length % 2;
-		byte[] bytes = new byte[byteCount];
-		for (int i = 0; i < chars.length; i++) {
-			try {
-				bytes[i / 2] |= hexCharToValue(chars[i]) << (4 * ((i + 1) % 2)) & 0xff;
-			} catch (IllegalArgumentException e) {
-				throw e;
-			}
-		}
-		return bytes;
-	}
+	
 
-	public static int valueToHexChar(int b) throws IllegalArgumentException {
-		try {
-			return valueToHexChar(b, false);
-		} catch (IllegalArgumentException e) {
-			throw e;
-		}
-	}
-
-	public static Integer valueToHexChar(int b, boolean upper)
+	private static Integer valueToHexChar(int b, boolean upper)
 			throws IllegalArgumentException {
 		char result;
 		switch (b) {
@@ -131,68 +94,5 @@ public class Hex {
 			throw new IllegalArgumentException();
 		}
 		return (int) result;
-	}
-
-	private static Integer hexCharToValue(int c) {
-		int result;
-		switch (c) {
-		case '0':
-			result = 0;
-			break;
-		case '1':
-			result = 1;
-			break;
-		case '2':
-			result = 2;
-			break;
-		case '3':
-			result = 3;
-			break;
-		case '4':
-			result = 4;
-			break;
-		case '5':
-			result = 5;
-			break;
-		case '6':
-			result = 6;
-			break;
-		case '7':
-			result = 7;
-			break;
-		case '8':
-			result = 8;
-			break;
-		case '9':
-			result = 9;
-			break;
-		case 'A':
-		case 'a':
-			result = 10;
-			break;
-		case 'B':
-		case 'b':
-			result = 11;
-			break;
-		case 'C':
-		case 'c':
-			result = 12;
-			break;
-		case 'D':
-		case 'd':
-			result = 13;
-			break;
-		case 'E':
-		case 'e':
-			result = 14;
-			break;
-		case 'F':
-		case 'f':
-			result = 15;
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-		return result;
 	}
 }
