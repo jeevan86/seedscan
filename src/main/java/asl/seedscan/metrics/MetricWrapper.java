ea64cd083b1cade6bf3c913db1e3d4b494f5ea4c
James Holland <jholland@usgs.gov>
2015-08-05 12:56:05 -0600

    Removed unused logger

--- src/main/java/asl/seedscan/metrics/MetricWrapper.java
+++ src/main/java/asl/seedscan/metrics/MetricWrapper.java
@@ -20,13 +20,7 @@ package asl.seedscan.metrics;
 
 import java.util.Enumeration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MetricWrapper {
-	private static final Logger logger = LoggerFactory
-			.getLogger(asl.seedscan.metrics.MetricWrapper.class);
-
 	private Metric arguments;
 	private Class<?> metricClass;
 
