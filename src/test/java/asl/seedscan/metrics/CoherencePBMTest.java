15a125c1e8bf30bb1c748651aabb64d792f0dc79
James Holland <jholland@usgs.gov>
2015-10-20 16:39:04 -0600

    Tweaked the coherence test case to not have LH1-LH1 results since they should not exist.

--- src/test/java/asl/seedscan/metrics/CoherencePBMTest.java
+++ src/test/java/asl/seedscan/metrics/CoherencePBMTest.java
@@ -43,8 +43,6 @@ public class CoherencePBMTest {
 		expect.put("00-10,LHZ-LHZ", 0.99997471849571);
 		expect.put("00-10,LHND-LHND", 0.9997343078305163);
 		expect.put("00-10,LHED-LHED", 0.9976266947968053);
-		expect.put("00-10,LH1-LH1", 0.5667547040224983);
-		expect.put("00-10,LH2-LH2", 0.557422800339938);
 		testMetric(metric, expect);
 
 		//TEST 18 - 22
@@ -56,8 +54,6 @@ public class CoherencePBMTest {
 		expect.put("00-10,LHZ-LHZ", 0.9996693481353688);
 		expect.put("00-10,LHND-LHND", 0.997686279191404);
 		expect.put("00-10,LHED-LHED", 0.9983404620924748);
-		expect.put("00-10,LH1-LH1", 0.5925009698602728);
-		expect.put("00-10,LH2-LH2", 0.7084211389935788);
 		testMetric(metric, expect);
 		
 		//TEST 90 - 110
@@ -68,9 +64,7 @@ public class CoherencePBMTest {
 		expect = new HashMap<String, Double>();
 		expect.put("00-10,LHZ-LHZ", 0.8466037396390191);
 		expect.put("00-10,LHND-LHND", 0.5581889516577663);
-		expect.put("00-10,LHED-LHED", 0.659323426099024);	
-		expect.put("00-10,LH1-LH1", 0.3831008902272484);
-		expect.put("00-10,LH2-LH2", 0.4012376564953167);
+		expect.put("00-10,LHED-LHED", 0.659323426099024);
 		testMetric(metric, expect);
 
 		//TEST 200 - 500
@@ -82,8 +76,6 @@ public class CoherencePBMTest {
 		expect.put("00-10,LHZ-LHZ", 0.2937884614962967);
 		expect.put("00-10,LHND-LHND", 0.21167174950454593);
 		expect.put("00-10,LHED-LHED",  0.21227611120383297);
-		expect.put("00-10,LH1-LH1", 0.22145105082997765);
-		expect.put("00-10,LH2-LH2", 0.2187485908321105);
 		testMetric(metric, expect);
 
 	}
