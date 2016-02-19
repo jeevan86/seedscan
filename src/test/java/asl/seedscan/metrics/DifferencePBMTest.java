1a3dc58442c92c941533c227f4fdded63250a34f
James Holland <jholland@usgs.gov>
2016-02-19 15:22:57 -0700

    Add new unit test for Difference Metric

---
+++ src/test/java/asl/seedscan/metrics/DifferencePBMTest.java
@@ -0,0 +1,42 @@
+package asl.seedscan.metrics;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DifferencePBMTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGetVersion() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testProcess() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testGetBaseName() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
