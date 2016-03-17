6b24107619b2cc1a3b9846714e2ebd83042ff298
James Holland <jholland@usgs.gov>
2016-03-16 19:48:09 -0600

    Reformat EventLoaderTest's source code

--- src/test/java/asl/seedscan/event/EventLoaderTest.java
+++ src/test/java/asl/seedscan/event/EventLoaderTest.java
@@ -33,27 +33,29 @@ public class EventLoaderTest {
 	@Test
 	public final void testEventLoader() throws Exception {
 		String correctEventDir = EventLoader.getEventsDirectory();
-		//Try to change the directory, shouldn't happen.
+		// Try to change the directory, shouldn't happen.
 		new EventLoader(ResourceManager.getDirectoryPath("/models"));
 		assertEquals(EventLoader.getEventsDirectory(), correctEventDir);
 	}
 
 	@Test
 	public final void testGetDaySynthetics() throws Exception {
-		Calendar date = new GregorianCalendar(2015,9,26); //GregorianCalendar is 0 based with months
+		Calendar date = new GregorianCalendar(2015, 9, 26); // GregorianCalendar
+															// is 0 based with
+															// months
 		Station station = new Station("IU", "NWAO");
-		
-		eventLoader.getDayEvents(date); //TODO: This shouldn't be required.
+
+		eventLoader.getDayEvents(date); // TODO: This shouldn't be required.
 		Hashtable<String, Hashtable<String, SacTimeSeries>> synthetics = eventLoader.getDaySynthetics(date, station);
 		assertNotNull(synthetics);
 		Hashtable<String, SacTimeSeries> eventSynthetics = synthetics.get("C201510260909A");
-		
+
 		SacTimeSeries timeseries = eventSynthetics.get("NWAO.XX.LXZ.modes.sac.proc");
 		assertEquals(new Integer(timeseries.getNumPtsRead()), new Integer(3999));
 		timeseries = eventSynthetics.get("NWAO.XX.LXZ.modes.sac");
 		assertEquals(new Integer(timeseries.getNumPtsRead()), new Integer(8000));
-		
-		//Should only return table with synthetics that are actually there.
+
+		// Should only return table with synthetics that are actually there.
 		station = new Station("IU", "MAKZ");
 		synthetics = eventLoader.getDaySynthetics(date, station);
 		assertTrue(synthetics.isEmpty());
@@ -62,12 +64,14 @@ public class EventLoaderTest {
 
 	@Test
 	public final void testGetDayEvents() throws Exception {
-		Calendar date = new GregorianCalendar(2015,9,26); //GregorianCalendar is 0 based with months
+		Calendar date = new GregorianCalendar(2015, 9, 26); // GregorianCalendar
+															// is 0 based with
+															// months
 		Hashtable<String, EventCMT> cmts = eventLoader.getDayEvents(date);
 		EventCMT cmt = cmts.get("C201510260909A");
-		assertEquals(new Double(cmt.getLatitude()), new Double(36.44) );
-		assertEquals(new Double(cmt.getLongitude()), new Double(70.72) );
-		assertEquals(new Double(cmt.getDepth()), new Double(212.5) );
+		assertEquals(new Double(cmt.getLatitude()), new Double(36.44));
+		assertEquals(new Double(cmt.getLongitude()), new Double(70.72));
+		assertEquals(new Double(cmt.getDepth()), new Double(212.5));
 	}
 
 }
