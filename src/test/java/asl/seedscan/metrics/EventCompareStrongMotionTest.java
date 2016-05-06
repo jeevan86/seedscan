f9c39ba1061b6566462137ac1a734a62b2a12cd0
James Holland <jholland@usgs.gov>
2016-05-06 16:54:41 -0600

    Updated the ECSM test code

--- src/test/java/asl/seedscan/metrics/EventCompareStrongMotionTest.java
+++ src/test/java/asl/seedscan/metrics/EventCompareStrongMotionTest.java
@@ -24,6 +24,7 @@ public class EventCompareStrongMotionTest {
 	public static void setUpBeforeClass() throws Exception {
 		try {
 			data = (MetricData) ResourceManager.loadCompressedObject("/data/IU.NWAO.2015.299.MetricData.ser.gz");
+			//data = (MetricData) ResourceManager.loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz");
 			eventLoader = new EventLoader(ResourceManager.getDirectoryPath("/events"));
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -50,11 +51,12 @@ public class EventCompareStrongMotionTest {
 		Calendar date = new GregorianCalendar(2015,9,26);
 		metric.setEventTable(eventLoader.getDayEvents(date));
 		HashMap<String, Double> expect = new HashMap<String, Double>();
-		expect.put("00-20,LHZ-LNZ", 2.884394926482693);
-		expect.put("00-20,LHND-LNND", 0.6848874383447533);
-		expect.put("00-20,LHED-LNED", 1.0097711288921811);
-		expect.put("10-20,LHZ-LNZ", 4.0);
-		expect.put("10-20,LHND-LNND", 4.0);
+		expect.put("00-20,LHZ-LNZ", 2.884394926482693);  //Was 2.884394926482693 before removing hard coding
+		expect.put("00-20,LHND-LNND", 0.6842270334452618); //Was 0.6848874383447533
+		expect.put("00-20,LHED-LNED", 1.0140182353130993); //1.0097711288921811
+		expect.put("10-20,LHZ-LNZ", 4.0); //4.0
+		expect.put("10-20,LHND-LNND", 4.0); //4.0
+		expect.put("10-20,LHED-LNED", -4.0); //Nonexistent
 		testMetric(metric, expect);
 
 	}
