8fddaada7bd9c43a1738f65436cfadd4d2b0ae26
James Holland <jholland@usgs.gov>
2016-05-31 17:18:58 -0600

    Create template for Event Compare synthetic test case

---
+++ src/test/java/asl/seedscan/metrics/EventCompareSyntheticTest.java
@@ -0,0 +1,84 @@
+package asl.seedscan.metrics;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import asl.seedscan.event.EventLoader;
+import asl.util.ResourceManager;
+
+public class EventCompareSyntheticTest {
+	private EventCompareSynthetic metric;
+	private static MetricData data;
+	private static EventLoader eventLoader;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		try {
+			data = (MetricData) ResourceManager.loadCompressedObject("/data/IU.NWAO.2015.299.MetricData.ser.gz", false);
+			//data = (MetricData) ResourceManager.loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz");
+			eventLoader = new EventLoader(ResourceManager.getDirectoryPath("/events"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		data = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testProcess() throws Exception {
+		/* The String key matches the MetricResult ids */
+		
+		//TEST 4 - 8
+		metric = new EventCompareSynthetic();
+		metric.setData(data);
+		Calendar date = new GregorianCalendar(2015,9,26);
+		metric.setEventTable(eventLoader.getDayEvents(date));
+		HashMap<String, Double> expect = new HashMap<String, Double>();
+		expect.put("00-20,LHZ-LNZ", 2.884394926482693);  //Was 2.884394926482693 before removing hard coding
+		expect.put("00-20,LHND-LNND", 0.6842270334452618); //Was 0.6848874383447533
+		expect.put("00-20,LHED-LNED", 1.0140182353130993); //1.0097711288921811
+		expect.put("10-20,LHZ-LNZ", 4.0); //4.0
+		expect.put("10-20,LHND-LNND", 4.0); //4.0
+		expect.put("10-20,LHED-LNED", -4.0); //Nonexistent
+		testMetric(metric, expect);
+
+	}
+	
+	public void testMetric(EventCompareSynthetic metric, HashMap<String, Double> expect) throws Exception {
+		metric.process();
+			MetricResult result = metric.getMetricResult();
+			for (String id : result.getIdSet()) {
+				Double expected = (double)Math.round(expect.get(id)       * 1000000d) / 1000000d;
+				Double resulted = (double)Math.round(result.getResult(id) * 1000000d) / 1000000d;
+				assertEquals(id + " result: ", expected, resulted);	
+			}
+	}
+
+	@Test
+	public final void testGetVersion() throws Exception {
+		metric = new EventCompareSynthetic();
+		assertEquals(1, metric.getVersion());
+	}
+
+	@Test
+	public final void testGetName() throws Exception {
+		metric = new EventCompareSynthetic();
+		assertEquals("EventCompareStrongMotion", metric.getName());
+	}
+}
