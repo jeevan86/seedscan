9bf745378a257e5ebe2fc2ae5e59958f00f4f391
James Holland <jholland@usgs.gov>
2017-01-13 19:17:09 -0700

    Migrate Station level scanning to StationScan and ...
    finalize comparisons between Station scans and ScanWorker objects

--- src/main/java/asl/seedscan/scanner/scanworker/ScanWorker.java
+++ src/main/java/asl/seedscan/scanner/scanworker/ScanWorker.java
@@ -1,24 +1,52 @@
-package asl.seedscan.scanner;
+package asl.seedscan.scanner.scanworker;
+
+import asl.seedscan.scanner.ScanManager;
 
 abstract class ScanWorker implements Comparable<ScanWorker>, Runnable {
-	
+
 	protected final ScanManager manager;
-	
-	ScanWorker(ScanManager manager){
+
+	ScanWorker(ScanManager manager) {
 		this.manager = manager;
 	}
 
-
 	/**
-	 * Returns the base priority. This is used for setting levels of running. EG Metrics run before Station scans. 
-	 * Lower values are lower priority.
-	 * Current priorities are as below.
-	 * 1 - Generic Scan
-	 * 2 - Station Scan
-	 * 3 - Metric
+	 * Returns the base priority. This is used for setting levels of running. EG
+	 * Metrics run before Station scans. Lower values are higher priority.
+	 * Standard base priorities are below. <br>
+	 * 55 - Generic Scan <br>
+	 * 45 - Station Scan <br>
+	 * 35 - Metric <br>
+	 * 
+	 * If a specialized ScanWorker needs to always run before or after other
+	 * workers of the same type, then its priority can vary within the ones
+	 * place. Other fine ordering can occur in getFinePriority()
 	 * 
 	 * @return the priority of this worker.
 	 */
-	abstract int getPriorityBase();
+	abstract Integer getBasePriority();
+
+	/**
+	 * This is used after base priority comparison. This is used to compare
+	 * workers of the same type. Its methodology may differ based on what child
+	 * ScanWorker type is being used.
+	 * 
+	 * @return
+	 */
+	abstract Long getFinePriority();
+
+	/**
+	 * This compares first the base priority, then the fine priority.
+	 * 
+	 * Children classes need only to convert themselves into a Long that is
+	 * suitable for comparison.
+	 */
+	public int compareTo(ScanWorker other) {
+		// Compare base priority first
+		if (!(this.getBasePriority().equals(other.getBasePriority()))) {
+			return this.getBasePriority().compareTo(other.getBasePriority());
+		}
+		return this.getFinePriority().compareTo(other.getFinePriority());
+	}
 
 }
--- src/main/java/asl/seedscan/scanner/scanworker/StationScan.java
+++ src/main/java/asl/seedscan/scanner/scanworker/StationScan.java
@@ -1,109 +1,217 @@
-package asl.seedscan.scanner;
+package asl.seedscan.scanner.scanworker;
 
 import java.time.LocalDate;
-import java.util.UUID;
+import java.time.format.DateTimeFormatter;
+import java.util.Hashtable;
 
-// TODO: Auto-generated Javadoc
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import asl.metadata.Station;
+import asl.metadata.meta_new.StationMeta;
+import asl.seedscan.Global;
+import asl.seedscan.database.DatabaseScan;
+import asl.seedscan.event.EventCMT;
+import asl.seedscan.event.EventLoader;
+import asl.seedscan.metrics.Metric;
+import asl.seedscan.metrics.MetricData;
+import asl.seedscan.metrics.MetricResult;
+import asl.seedscan.metrics.MetricWrapper;
+import asl.seedscan.scanner.DataLoader;
+import asl.seedscan.scanner.ScanManager;
+import asl.timeseries.CrossPower;
+import asl.timeseries.CrossPowerKey;
+import sac.SacTimeSeries;
+
 /**
  * The Class Scan.
  */
 public class StationScan extends ScanWorker {
-	
-	protected final UUID scanID;
-	
-	protected final UUID parentScanID;
+	private static final Logger logger = LoggerFactory.getLogger(asl.seedscan.scanner.scanworker.StationScan.class);
 
-
-	/** The start date. */
-	protected final LocalDate startDate;
+	private final Station station;
+	private final DatabaseScan databaseScan;
 	
-	/** The end date. */
-	protected final LocalDate endDate;
+	private final LocalDate currentDate;
 	
-	/** True if the scan requires deletion of existing data. */
-	protected final boolean deleteExisting;
-
-	/* Scope of Scan */
-	/** Restrict to this network */
-	protected final String network;
+	/**
+	 * Data for the day being scanned.
+	 */
+	private MetricData currentMetricData;
+	/**
+	 * Data for the day that chronologically follows day being scanned.
+	 */
+	private MetricData nextMetricData;
 	
-	/** Restrict to this station */
-	protected final String station;
-	
-	/** Used for deleting existing data. */
-	protected final String location;
-	
-	/** Used for deleting existing data. */
-	protected final String channel;
-	
-	/** Only recompute this metric */
-	protected final String metricName;
 
 	/**
-	 * Instantiates a new scan.
-	 *
-	 * @param startDate the start date
-	 * @param endDate the end date
-	 * @param deleteExisting the delete existing
-	 * @param network the network
-	 * @param station the station
-	 * @param location the location
-	 * @param channel the channel
-	 * @param metricName the metric name
+	 * Start the first day of the scan
+	 * @param manager
+	 * @param databaseScan
 	 */
-	//@formatter:off
-	public StationScan(
-		UUID scanID,
-		UUID parentScanID,
-		String metricName,
-		String network,
-		String station,
-		String location,
-		String channel,
-		LocalDate startDate,
-		LocalDate endDate,
-		boolean deleteExisting,
-		ScanManager manager
-	) {
+	StationScan(ScanManager manager, DatabaseScan databaseScan) {
 		super(manager);
-		this.scanID = scanID;
-		this.parentScanID = parentScanID;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.deleteExisting = deleteExisting;
-
-		this.network = network;
-		this.station = station;
-		this.location = location;
-		this.channel = channel;
-		this.metricName = metricName;
+		this.station = new Station(databaseScan.network, databaseScan.station);
+		this.databaseScan = databaseScan;
+		this.currentDate = databaseScan.startDate;
 	}
-	//@formatter:on
+
+	/**
+	 * Scan a specified day with provided metricData
+	 * @param manager
+	 * @param databaseScan
+	 * @param date The day to scan
+	 * @param metricData The days preloaded MetricData. Can be null
+	 */
+	StationScan(ScanManager manager, DatabaseScan databaseScan, LocalDate date, MetricData metricData) {
+		super(manager);
+		this.station = new Station(databaseScan.network, databaseScan.station);
+		this.databaseScan = databaseScan;
+		this.currentDate = date;
+		this.currentMetricData = metricData;
+	}
+
 
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		logger.debug("Scan Station={} Day={} Thread id=[{}]", station,
+				currentDate.format(DateTimeFormatter.ISO_ORDINAL_DATE), Thread.currentThread().getId());
+		
+		// CMT Event loader - use to load events for each day
+		EventLoader eventLoader = new EventLoader(Global.CONFIG.getEventsDir());
 
-	}
+		LocalDate nextDayTimestamp = currentDate.plusDays(1);
 
-	/* (non-Javadoc)
-	 * @see java.lang.Comparable#compareTo(java.lang.Object)
-	 */
-	@Override
-	public int compareTo(ScanWorker o) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Get all the channel metadata for this station, for this day
+		StationMeta stnMeta = manager.metaGenerator.getStationMeta(station, currentDate.atStartOfDay());
+
+		Hashtable<String, Hashtable<String, SacTimeSeries>> eventSynthetics = null;
+		
+		Hashtable<String, EventCMT> eventCMTs = eventLoader.getDayEvents(currentDate);
+		if (eventCMTs != null) {
+			eventSynthetics = eventLoader.getDaySynthetics(currentDate, station);
+		}
+		
+		//May have been passed from previous day
+		if(currentMetricData == null){
+			currentMetricData = DataLoader.getMetricData(currentDate, station, manager);
+		}
+		nextMetricData = DataLoader.getMetricData(nextDayTimestamp, station, manager);
+
+		if (currentMetricData != null) {
+			// This doesn't mean nextMetricData isn't null!
+			currentMetricData.setNextMetricData(nextMetricData);
+		}
+		
+		// No Metadata found for this station-day --> skip day
+		if (stnMeta == null) {
+			logger.info("== Scanner: No Metadata found for Station:{}_{} for Day:{} --> Skipping", station.getNetwork(),
+					station.getStation(), currentDate.format(DateTimeFormatter.ISO_ORDINAL_DATE));
+		} else {
+
+			stnMeta.printStationInfo();
+
+			// Loop over Metrics to compute, for this station, for this day
+			Hashtable<CrossPowerKey, CrossPower> crossPowerMap = null;
+
+			try { // wrapper.getNewInstance()
+				for (MetricWrapper wrapper : Global.METRICS) {
+					Metric metric = wrapper.getNewInstance();
+					metric.setBaseOutputDir(Global.CONFIG.getPlotsDir());
+
+					if (currentMetricData == null) {
+						metric.setData(new MetricData(manager.database, stnMeta));
+					} else {
+						metric.setData(currentMetricData);
+					}
+					if (eventCMTs != null) {
+						metric.setEventTable(eventCMTs);
+						if (eventSynthetics != null) {
+							metric.setEventSynthetics(eventSynthetics);
+						}
+					}
+
+					// Hand off the crossPowerMap from metric to metric,
+					// adding to it each time
+					if (crossPowerMap != null) {
+						metric.setCrossPowerMap(crossPowerMap);
+					}
+					metric.process();
+					// Save the current crossPowerMap for the next metric:
+					crossPowerMap = metric.getCrossPowerMap();
+
+					// This is a little convoluted: calibration.getResult()
+					// returns a MetricResult, which may contain many values
+					// in a Hashtable<String,String> = map.
+					// MetricResult.getResult(id) returns value = String
+
+					MetricResult results = metric.getMetricResult();
+					if (results == null) {
+					} else {
+						for (String id : results.getIdSortedSet()) {
+							double value = results.getResult(id);
+						/* @formatter:off */
+						logger.info(String.format("%s [%7s] [%s] %15s:%6.2f", results.getMetricName(),
+								results.getStation(), results.getDate().format(DateTimeFormatter.ISO_ORDINAL_DATE), id, value));
+
+						if (Double.isNaN(value)) {
+							logger.error(String.format("%s [%s] [%s] %s: ERROR: metric value = [ NaN ] !!\n",
+									results.getMetricName(), results.getStation(),
+									results.getDate().format(DateTimeFormatter.ISO_ORDINAL_DATE), id));
+						}
+						if (Double.isInfinite(value)) {
+							logger.error(String.format("%s [%s] [%s] %s: ERROR: metric value = [ Infinity ] !!\n",
+									results.getMetricName(), results.getStation(),
+									results.getDate().format(DateTimeFormatter.ISO_ORDINAL_DATE), id));
+						}
+						/* @formatter:on */
+						}
+						if (manager.database.isConnected()) {
+							manager.database.insertMetricData(results);
+						} else {
+							logger.warn("Injector *IS NOT* connected --> Don't inject");
+						}
+					}
+				} // end loop over metrics
+			} catch (InstantiationException e) {
+				logger.error("Scanner InstantationException:", e);
+			} catch (IllegalAccessException e) {
+				logger.error("Scanner IllegalAccessException:", e);
+			} catch (NoSuchFieldException e) {
+				logger.error("Scanner NoSuchFieldException:", e);
+			} catch (IllegalArgumentException e) {
+				logger.error("Scanner IllegalArgumentException:", e);
+			}
+		}
+			
+		//Insert Next Day task
+		if(nextDayTimestamp.compareTo(databaseScan.endDate) <= 0){
+			manager.threadPool.submit(new StationScan(this.manager, this.databaseScan, nextDayTimestamp, this.nextMetricData));
+		}
+
+		//Cleanup
+		/* Need to null out ref to next day before passing currentMetricData to avoid chaining refs*/
+		if (currentMetricData != null) {
+			currentMetricData.setNextMetricDataToNull();
+		}
+		currentMetricData = null;
+		nextMetricData = null;
 	}
 
 	/* (non-Javadoc)
 	 * @see asl.seedscan.worker.Worker#getPriorityBase()
 	 */
 	@Override
-	public int getPriorityBase() {
-		return 1;
+	public Integer getBasePriority() {
+		return 45;
+	}
+	
+	public Long getFinePriority() {
+		//We want to prioritize dates when comparing station scans
+		return this.currentDate.toEpochDay();
 	}
 
 }
