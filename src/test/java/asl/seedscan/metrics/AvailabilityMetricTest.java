a5c35e98e2c70be862f27bda9101770362d95790
James Holland <jholland@usgs.gov>
2017-01-27 12:58:31 -0700

    Add placeholder tests for Availability Metric

---
+++ src/test/java/asl/seedscan/metrics/AvailabilityMetricTest.java
@@ -0,0 +1,22 @@
+package asl.seedscan.metrics;
+
+import org.junit.Test;
+
+public class AvailabilityMetricTest {
+
+	@Test
+	public final void testGetVersion() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testGetName() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testProcess() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
