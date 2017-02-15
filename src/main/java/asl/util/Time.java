b574a9ea5115c26d407a3679a6bc0e8c51b9700d
James Holland <jholland@usgs.gov>
2017-02-15 12:57:40 -0700

    Add helper functions for converting SEED BTIME to LocalDateTime

--- src/main/java/asl/util/Time.java
+++ src/main/java/asl/util/Time.java
@@ -1,7 +1,9 @@
 package asl.util;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 
@@ -34,5 +36,34 @@ public class Time {
 	public static long calculateEpochMilliSeconds(LocalDateTime timestamp) {
 		Instant startInstant = timestamp.atZone(ZoneId.ofOffset("", ZoneOffset.UTC)).toInstant();
 		return startInstant.toEpochMilli();
+	}
+
+	/**
+	 * This method converts from the seed Convention that uses 0.0001 seconds (decimilliseconds)
+	 * to nanoseconds for use with more standard applications.
+	 * @param dmSeconds 0.0001 seconds
+	 * @return the nanosecond conversion
+	 */
+	public static int decimillisecondsToNanoSeconds(int dmSeconds){
+		return dmSeconds * 100000;
+	}
+
+	/**
+	 * Creates a LocalDateTime object from the values in a BTIME field in the SEED format.
+	 * See Chapter 3 in Seed Manual
+	 * @param year Year (e.g., 1987)
+	 * @param dayOfYear Day of Year (Jan 1 is 1)
+	 * @param hour Hours of day (0-23)
+	 * @param minute Minutes of day (0-59)
+	 * @param second Seconds of day (0-59, 60 for leap seconds)
+	 * @param dmSecond 0.0001 seconds (0-9999)
+	 * @return the LocalDateTime version of provided data
+	 */
+	public static LocalDateTime btimeToLocalDateTime(int year, int dayOfYear, int hour, int minute, int second, int dmSecond){
+		LocalDate date = LocalDate.ofYearDay(year, dayOfYear);
+		LocalTime time = LocalTime.of(hour, minute, second, Time.decimillisecondsToNanoSeconds(dmSecond));
+
+		return LocalDateTime.of(date, time);
+
 	}
 }
---
+++ src/test/java/asl/util/TimeTest.java
@@ -0,0 +1,48 @@
+package asl.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by jholland on 2/15/17.
+ */
+public class TimeTest {
+
+  @Test
+  public void calculateEpochMicroSeconds() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+  @Test
+  public void calculateEpochMilliSeconds() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+  @Test
+  public void decimillisecondsToNanoSeconds() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+  @Test
+  public void btimeToLocalDateTime_Leap_Second() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+  @Test
+  public void btimeToLocalDateTime_Standard() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+  @Test
+  public void btimeToLocalDateTime_Leap_Year() throws Exception {
+    throw new RuntimeException("not yet implemented");
+
+  }
+
+}
