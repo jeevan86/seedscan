2cd39b69ba4ddb3dcc455c967dd952442b9bd987
James Holland <jamesfholland@gmail.com>
2017-02-17 18:14:00 -0700

    Add some basic tests for Availability

--- src/test/java/asl/seedscan/metrics/AvailabilityMetricTest.java
+++ src/test/java/asl/seedscan/metrics/AvailabilityMetricTest.java
@@ -1,12 +1,38 @@
 package asl.seedscan.metrics;
 
+import static org.junit.Assert.assertEquals;
+
+import asl.testutils.ResourceManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AvailabilityMetricTest {
 
+	private AvailabilityMetric metric;
+	private static MetricData data;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		data = (MetricData) ResourceManager
+				.loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", false);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		data = null;
+	}
+
 	@Test
 	public final void testGetName() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		Metric metric = new AvailabilityMetric();
+		assertEquals("AvailabilityMetric", metric.getName());
+	}
+
+	@Test
+	public final void testGetVersion() throws Exception {
+		Metric metric = new AvailabilityMetric();
+		assertEquals(1, metric.getVersion());
 	}
 
 	@Test
