621f24b1c1ef8e1e5b3bc5eda5adfa454a528f6a
James Holland <jamesfholland@gmail.com>
2017-02-17 18:14:41 -0700

    Reformat avail tests to google style

--- src/test/java/asl/seedscan/metrics/AvailabilityMetricTest.java
+++ src/test/java/asl/seedscan/metrics/AvailabilityMetricTest.java
@@ -9,47 +9,49 @@ import org.junit.Test;
 
 public class AvailabilityMetricTest {
 
-	private AvailabilityMetric metric;
-	private static MetricData data;
+  private AvailabilityMetric metric;
+  private static MetricData data;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		data = (MetricData) ResourceManager
-				.loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", false);
-	}
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    data = (MetricData) ResourceManager
+        .loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", false);
+  }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		data = null;
-	}
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    data = null;
+  }
 
-	@Test
-	public final void testGetName() throws Exception {
-		Metric metric = new AvailabilityMetric();
-		assertEquals("AvailabilityMetric", metric.getName());
-	}
+  @Test
+  public final void testGetName() throws Exception {
+    Metric metric = new AvailabilityMetric();
+    assertEquals("AvailabilityMetric", metric.getName());
+  }
 
-	@Test
-	public final void testGetVersion() throws Exception {
-		Metric metric = new AvailabilityMetric();
-		assertEquals(1, metric.getVersion());
-	}
+  @Test
+  public final void testGetVersion() throws Exception {
+    Metric metric = new AvailabilityMetric();
+    assertEquals(1, metric.getVersion());
+  }
 
-	@Test
-	public final void testProcess_HasData_NoDB() throws Exception {
-		throw new RuntimeException("not yet implemented");
-	}
-	@Test
-	public final void testProcess_HasData_HasDB() throws Exception {
-		throw new RuntimeException("not yet implemented");
-	}
-	
-	@Test
-	public final void testProcess_NoData_NoDB() throws Exception {
-		throw new RuntimeException("not yet implemented");
-	}
-	@Test
-	public final void testProcess_NoData_HasDB() throws Exception {
-		throw new RuntimeException("not yet implemented");
-	}
+  @Test
+  public final void testProcess_HasData_NoDB() throws Exception {
+    throw new RuntimeException("not yet implemented");
+  }
+
+  @Test
+  public final void testProcess_HasData_HasDB() throws Exception {
+    throw new RuntimeException("not yet implemented");
+  }
+
+  @Test
+  public final void testProcess_NoData_NoDB() throws Exception {
+    throw new RuntimeException("not yet implemented");
+  }
+
+  @Test
+  public final void testProcess_NoData_HasDB() throws Exception {
+    throw new RuntimeException("not yet implemented");
+  }
 }
