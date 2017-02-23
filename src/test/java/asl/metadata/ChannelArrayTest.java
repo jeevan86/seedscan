a0d4c99d9d502da10e215fb57ddb9affbddda2ac
James Holland <jamesfholland@gmail.com>
2017-02-22 18:07:25 -0700

    Add unit tests for ChannelArray

--- src/test/java/asl/metadata/ChannelArrayTest.java
+++ src/test/java/asl/metadata/ChannelArrayTest.java
@@ -1,7 +1,9 @@
 package asl.metadata;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
 import org.junit.Test;
 
 public class ChannelArrayTest {
@@ -9,22 +11,38 @@ public class ChannelArrayTest {
 
   @Test
   public void testChannelArray_Location_ThreeStringChannel() throws Exception {
-    throw new RuntimeException("not yet implemented");
+    ChannelArray array = new ChannelArray("00", "LH1", "LH2", "LHZ");
+    List<Channel> list = array.getChannels();
+    assertTrue(list.contains(new Channel("00", "LH1")));
+    assertTrue(list.contains(new Channel("00", "LH2")));
+    assertTrue(list.contains(new Channel("00", "LHZ")));
+    assertEquals(3, list.size());
   }
 
   @Test
   public void testChannelArray_Location_OneStringChannel() throws Exception {
-    throw new RuntimeException("not yet implemented");
+    ChannelArray array = new ChannelArray("10", "LHZ");
+    List<Channel> list = array.getChannels();
+    assertTrue(list.contains(new Channel("10", "LHZ")));
+    assertEquals(1, list.size());
   }
 
   @Test
   public void testChannelArray_TwoChannel() throws Exception {
-    throw new RuntimeException("not yet implemented");
+    Channel a = new Channel("00", "LH1");
+    Channel b = new Channel("10", "LH2");
+    ChannelArray array = new ChannelArray(a, b);
+    List<Channel> list = array.getChannels();
+    assertTrue(list.contains(a));
+    assertTrue(list.contains(b));
+    assertEquals(2, list.size());
   }
 
-  @Test
-  public void getChannels() throws Exception {
-    throw new RuntimeException("not yet implemented");
+  @Test(expected = UnsupportedOperationException.class)
+  public void getChannels_Unmodifiable() throws Exception {
+    ChannelArray array = new ChannelArray("00", "LH1", "LH2", "LHZ");
+    List<Channel> list = array.getChannels();
+    list.add(new Channel("10", "LH1"));
   }
 
 }
