14a9f30e4a9cdf62af28023c036d266fc3a8690d
James Holland <jamesfholland@gmail.com>
2017-02-22 20:28:50 -0700

    Redo unit tests for MetricData

--- src/test/java/asl/seedscan/metrics/MetricDataTest.java
+++ src/test/java/asl/seedscan/metrics/MetricDataTest.java
@@ -57,10 +57,37 @@ public class MetricDataTest {
         new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel), 0.00,
         ByteBuffer.wrap("Different".getBytes()));
 
+    expectMetricName = "AnyMetric";
+    expectChannel = new Channel("00", "LHZ");
+    database.insertMockData(
+        new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel),
+        35.123456, ByteBuffer.wrap("Same1".getBytes()));
+
+    expectChannel = new Channel("00", "LH1");
+    database.insertMockData(
+        new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel),
+        99.999, ByteBuffer.wrap("Same1".getBytes()));
+
+    expectChannel = new Channel("00", "LH2");
+    database.insertMockData(
+        new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel), 0.00,
+        ByteBuffer.wrap("Different1".getBytes()));
+
     // ANMO - Digest only tests
+    expectMetricName = "AvailabilityMetric";
     expectDate = LocalDate.parse("2015-08-16");
     expectStation = new Station("IU", "ANMO");
 
+    expectChannel = new Channel("10", "BH1"); // Precomputed the digest
+    database.insertMockDigest(
+        new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel),
+        ByteBuffer.wrap(DatatypeConverter.parseHexBinary("9A4FE3A10FD60F93526F464B0DB9580E")));
+    expectChannel = new Channel("00", "LH2");
+    database.insertMockDigest(
+        new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel),
+        ByteBuffer.wrap("Different".getBytes()));
+
+    expectMetricName = "AnyMetric";
     expectChannel = new Channel("10", "BH1"); // Precomputed the digest
     database.insertMockDigest(
         new MetricValueIdentifier(expectDate, expectMetricName, expectStation, expectChannel),
@@ -86,7 +113,7 @@ public class MetricDataTest {
    * class.
    */
   @Test
-  public final void testGetMetaData() throws Exception {
+  public final void testGetMetaData_BasicInformationCorrect() throws Exception {
     StationMeta metadata = data.getMetaData();
     assertNotNull(metadata);
     assertEquals("2015:206", metadata.getDate());
@@ -94,18 +121,17 @@ public class MetricDataTest {
     assertEquals("IU_ANMO", metadata.toString());
   }
 
-  /*
-   * Checks various types of channels that should exist or do not exist in
-   * this data.
-   */
   @Test
-  public final void testHasChannels() throws Exception {
+  public final void testHasChannels_Exist() throws Exception {
     // Should exist
     assertTrue(data.hasChannels("00", "LH"));
     assertTrue(data.hasChannels("10", "BH"));
     // Double Check
     assertTrue(data.hasChannels("10", "BH"));
+  }
 
+  @Test
+  public final void testHasChannels_Nonexistent() throws Exception {
     // Should not exist
     assertFalse(data.hasChannels("70", "BH"));
     // 20-HNZ closed before this date
@@ -120,7 +146,7 @@ public class MetricDataTest {
    * Basic metric reader return value test and check if reader disconnected.
    */
   @Test
-  public final void testGetMetricValue() throws Exception {
+  public final void testGetMetricValue_KnownValues() throws Exception {
     MetricData metricData = new MetricData(database, metadata);
 
     LocalDate date = LocalDate.parse("2014-07-12");
@@ -134,9 +160,29 @@ public class MetricDataTest {
     Double expected = (double) Math.round(35.123456 * 1000000d) / 1000000d;
     Double resulted = (double) Math.round(value * 1000000d) / 1000000d;
     assertEquals(expected, resulted);
+  }
 
+  @Test
+  public final void testGetMetricValue_Disconnected() throws Exception {
+    //Mock Data
+    MetricDatabaseMock tempDatabase = new MetricDatabaseMock();
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "RandomMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    tempDatabase.insertMockData(
+        new MetricValueIdentifier(date, metricName, station, channel),
+        35.123456, ByteBuffer.wrap("OtherBytes".getBytes()));
     // Check disconnected reader
-    database.setConnected(false);
+
+    MetricData metricData = new MetricData(tempDatabase, metadata);
+    //Should be there
+    assertNotNull(metricData.getMetricValue(date, metricName, station, channel));
+
+    //Disconnect
+    tempDatabase.setConnected(false);
+    //Now it shouldn't
     assertNull(metricData.getMetricValue(date, metricName, station, channel));
   }
 
@@ -199,89 +245,271 @@ public class MetricDataTest {
 
   }
 
+  /*
+   * The tests for valueDigestChanged follow this pattern:
+   * Data |DigestInDB |ForceUpdate|AvailabilityMetric |Expect Null response
+   * 0    |0          |0          |0                  |1
+   * 0    |0          |0          |1                  |0
+   * 0    |0          |1          |0                  |1
+   * 0    |0          |1          |1                  |0
+   * 0    |1          |0          |0                  |1
+   * 0    |1          |0          |1                  |1
+   * 0    |1          |1          |0                  |1
+   * 0    |1          |1          |1                  |0
+   */
   @Test
-  public final void testValueDigestChanged() throws Exception {
-    // NO DATA
+  public final void testValueDigestChanged_NoData_NoDigestDatabase_NoForceUpdate_NotAvailability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "NotAvailMetric";
+    Channel channel = new Channel("00", "BH1"); // Not set in reader
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            false);
+    //Nothing to compute so null
+    assertNull(digest);
+  }
+
+  @Test
+  public final void testValueDigestChanged_NoData_NoDigestDatabase_NoForceUpdate_Availability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AvailabilityMetric";
+    Channel channel = new Channel("00", "BH1"); // Not set in reader
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            false);
+    //Availability computes regardless of No Data so not null
+    assertNotNull(digest);
+  }
+
+  @Test
+  public final void testValueDigestChanged_NoData_NoDigestDatabase_ForceUpdate_NotAvailability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AnyMetric";
+
+    Channel channel = new Channel("00", "BH1"); // Not set in reader
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            true);
+    //No data so nothing to compute
+    assertNull(digest);
+  }
+
+  @Test
+  public final void testValueDigestChanged_NoData_NoDigestDatabase_ForceUpdate_Availability()
+      throws Exception {
     MetricData metricData = new MetricData(database, metadata);
 
     LocalDate date = LocalDate.parse("2014-07-12");
     Station station = new Station("CU", "BCIP");
     String metricName = "AvailabilityMetric";
 
-    // No data, digest in database, no force update
+    Channel channel = new Channel("00", "BH1"); // Not set in reader
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            true);
+    //Availability with forced update, should compute
+    assertNotNull(digest);
+  }
+
+
+  @Test
+  public final void testValueDigestChanged_NoData_DigestDatabase_NoForceUpdate_NotAvailability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AnyMetric";
+
     Channel channel = new Channel("00", "LHZ");
     ByteBuffer digest = metricData.valueDigestChanged(channel,
         new MetricValueIdentifier(date, metricName, station, channel), false);
+    //No data and not availability don't care if already computed, don't recompute.
     assertNull(digest);
+  }
 
-    // No data, digest not in database, no force update
-    // Should come back with something.
-    channel = new Channel("00", "BH1"); // Not set in reader
-    digest = metricData
-        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
-            false);
-    assertNotNull(digest);
+  @Test
+  public final void testValueDigestChanged_NoData_DigestDatabase_NoForceUpdate_Availability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
 
-    // No data, digest in Reader, force update
-    channel = new Channel("00", "LHZ");
-    digest = metricData
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AvailabilityMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    ByteBuffer digest = metricData.valueDigestChanged(channel,
+        new MetricValueIdentifier(date, metricName, station, channel), false);
+    //Should be null, since database has availability, it might be temporary data.
+    assertNull(digest);
+  }
+
+  @Test
+  public final void testValueDigestChanged_NoData_DigestDatabase_ForceUpdate_NotAvailability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AnyMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             true);
-    assertNotNull(digest);
+    //No data, can't compute
+    assertNull(digest);
+  }
 
-    // No data, digest in Reader, reader disconnected
-    channel = new Channel("00", "LHZ");
-    database.setConnected(false);
-    digest = metricData
+  @Test
+  public final void testValueDigestChanged_NoData_DigestDatabase_ForceUpdate_Availability()
+      throws Exception {
+    MetricData metricData = new MetricData(database, metadata);
+
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AvailabilityMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            true);
+    //Force Update overrides digest in database should recompute.
+    assertNotNull(digest);
+  }
+
+  /*
+   * Disconnected Database test without data.
+   */
+  @Test
+  public final void testValueDigestChanged_NoData_DatabaseDisconnected_NoForceUpdate_NotAvailability()
+      throws Exception {
+    //Mock Data
+    MetricDatabaseMock tempDatabase = new MetricDatabaseMock();
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    /*Metric name should match exact this should not count as AvailabilityMetric*/
+    String metricName = "NotAvailabilityMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    tempDatabase.insertMockData(
+        new MetricValueIdentifier(date, metricName, station, channel),
+        35.123456, ByteBuffer.wrap("OtherBytes".getBytes()));
+    MetricData metricData = new MetricData(tempDatabase, metadata);
+
+    tempDatabase.setConnected(false);
+
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             false);
+    //No data, not Availability, can't compute
+    assertNull(digest);
+  }
+
+  @Test
+  public final void testValueDigestChanged_NoData_DatabaseDisconnected_NoForceUpdate_Availability()
+      throws Exception {
+    //Mock Data
+    MetricDatabaseMock tempDatabase = new MetricDatabaseMock();
+    LocalDate date = LocalDate.parse("2014-07-12");
+    Station station = new Station("CU", "BCIP");
+    String metricName = "AvailabilityMetric";
+
+    Channel channel = new Channel("00", "LHZ");
+    tempDatabase.insertMockData(
+        new MetricValueIdentifier(date, metricName, station, channel),
+        35.123456, ByteBuffer.wrap("OtherBytes".getBytes()));
+    MetricData metricData = new MetricData(tempDatabase, metadata);
+    // No data, digest in Reader, reader disconnected Non Availability Metric
+    tempDatabase.setConnected(false);
+    ByteBuffer digest = metricData
+        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
+            false);
+    //This should exist because it is availability
     assertNotNull(digest);
+  }
 
-    database.setConnected(true);
 
-    // DATA
-    metricData = (MetricData) ResourceManager
+  @Test
+  public final void testValueDigestChanged_Data_NoDigestDatabase_NoForceUpdate() throws Exception {
+    MetricData metricData = (MetricData) ResourceManager
         .loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", true);
     metricData.setMetricReader(database);
-    date = LocalDate.parse("2015-08-16");
+    LocalDate date = LocalDate.parse("2015-08-16");
 
-    station = new Station("IU", "ANMO");
-    metricName = "AvailabilityMetric";
-
-    // Data, no digest in Reader, no force update
-    channel = new Channel("10", "BH2");
-    digest = metricData
+    Station station = new Station("IU", "ANMO");
+    String metricName = "AnyMetric";
+    Channel channel = new Channel("10", "BH2");
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             false);
     assertNotNull(digest);
+  }
 
-    // Data, digest match in Reader, no force update
-    channel = new Channel("10", "BH1");
-    digest = metricData
+  @Test
+  public final void testValueDigestChanged_Data_MatchDigestDatabase_NoForceUpdate()
+      throws Exception {
+    MetricData metricData = (MetricData) ResourceManager
+        .loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", true);
+    metricData.setMetricReader(database);
+    LocalDate date = LocalDate.parse("2015-08-16");
+
+    Station station = new Station("IU", "ANMO");
+    String metricName = "AnyMetric";
+    Channel channel = new Channel("10", "BH1");
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             false);
+    //Digest match don't recompute
     assertNull(digest);
+  }
 
-    // Data, digest match in Reader, force update
-    digest = metricData
+  @Test
+  public final void testValueDigestChanged_Data_MatchDigestDatabase_ForceUpdate() throws Exception {
+    MetricData metricData = (MetricData) ResourceManager
+        .loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", true);
+    metricData.setMetricReader(database);
+    LocalDate date = LocalDate.parse("2015-08-16");
+
+    Station station = new Station("IU", "ANMO");
+    String metricName = "AnyMetric";
+    Channel channel = new Channel("10", "BH1");
+
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             true);
+    //Force update, so recompute
     assertNotNull(digest);
+  }
 
-    // Data, digest match in Reader, reader disconnected
-    database.setConnected(false);
-    digest = metricData
+  @Test
+  public final void testValueDigestChanged_Data_MismatchDigestDatabase_NoForceUpdate()
+      throws Exception {
+    MetricData metricData = (MetricData) ResourceManager
+        .loadCompressedObject("/data/IU.ANMO.2015.206.MetricData.ser.gz", true);
+    metricData.setMetricReader(database);
+    LocalDate date = LocalDate.parse("2015-08-16");
+
+    Station station = new Station("IU", "ANMO");
+    String metricName = "AnyMetric";
+
+    Channel channel = new Channel("00", "LH2");
+    ByteBuffer digest = metricData
         .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
             false);
+    //Digest Mismatch recompute
     assertNotNull(digest);
-    database.setConnected(true);
-
-    // Data, digest mismatch in Reader, no force update
-    channel = new Channel("00", "LH2");
-    digest = metricData
-        .valueDigestChanged(channel, new MetricValueIdentifier(date, metricName, station, channel),
-            false);
-    assertNotNull(digest);
-
   }
 }
