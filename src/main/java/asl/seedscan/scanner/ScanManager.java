1a1a9f923e8d772581f2ac873c3285f528dba6ec
James Holland <jholland@usgs.gov>
2017-03-07 18:38:17 -0700

    Reformat and make ScanManager pass tests

--- src/main/java/asl/seedscan/scanner/ScanManager.java
+++ src/main/java/asl/seedscan/scanner/ScanManager.java
@@ -15,71 +15,110 @@ import asl.seedscan.scanner.scanworker.ScanWorker;
 import asl.util.Logging;
 
 public class ScanManager {
-	private static final Logger logger = LoggerFactory.getLogger(asl.seedscan.scanner.ScanManager.class);
 
-	public final MetricDatabase database;
-	public final MetaGenerator metaGenerator;
+  private static final Logger logger = LoggerFactory
+      .getLogger(asl.seedscan.scanner.ScanManager.class);
 
-	/**
-	 * Pool of threads for scanning. Must remain private to restrict how it is
-	 * used. Because it is implemented with a PriorityBlockingQueue its tasks
-	 * must also implement Comparable.
-	 */
-	private final ThreadPoolExecutor threadPool;
-	
-	private final BlockingQueue<Runnable> workQueue;
+  public final MetricDatabase database;
+  public final MetaGenerator metaGenerator;
 
-	public ScanManager(MetricDatabase database, MetaGenerator metaGenerator) {
-		this.database = database;
-		this.metaGenerator = metaGenerator;
+  private long queryWaitTime = 300000;
+  private Boolean running = false;
+  private Thread scanThread;
 
-		int threadCount = Runtime.getRuntime().availableProcessors();
-		logger.info("Number of Threads to Use = [{}]", threadCount);
+  /**
+   * Pool of threads for scanning. Must remain private to restrict how it is
+   * used. Because it is implemented with a PriorityBlockingQueue its tasks
+   * must also implement Comparable.
+   */
+  private final ThreadPoolExecutor threadPool;
 
-		 workQueue = new PriorityBlockingQueue<>();
+  private final BlockingQueue<Runnable> workQueue;
 
-		this.threadPool = new ThreadPoolExecutor(threadCount, threadCount, 10, TimeUnit.MINUTES, workQueue);
-	}
+  public ScanManager(MetricDatabase database, MetaGenerator metaGenerator) {
+    this.database = database;
+    this.metaGenerator = metaGenerator;
 
-	/**
-	 * Begins the scan process. This blocks indefinitely while scans are being
-	 * performed.
-	 */
-	public void scan() {
-		//Start full set of threads going.
-		for(int i = 0; i <=this.threadPool.getCorePoolSize(); i++){
-			threadPool.execute(new RetrieveScan(this));
-		}
-		
-		while (true) {
-			if(workQueue.isEmpty()){
-				//Since queue is empty add a retrieving scan.
-				threadPool.execute(new RetrieveScan(this));
-			}
-			try {
-				/*We want to wait a little bit so as to not overload the db with getScan requests.*/
-				Thread.sleep(300000);
+    int threadCount = Runtime.getRuntime().availableProcessors();
+    logger.info("Number of Threads to Use = [{}]", threadCount);
+
+    workQueue = new PriorityBlockingQueue<>();
+
+    this.threadPool = new ThreadPoolExecutor(threadCount, threadCount, 10, TimeUnit.MINUTES,
+        workQueue);
+  }
+
+  /**
+   * Begins the scan process. This blocks indefinitely while scans are being
+   * performed.
+   *
+   * This method can only be called once.
+   *
+   * @throws IllegalStateException if multiple calls to scan were initiated.
+   */
+  public void scan() {
+    synchronized (this) {
+      if (running || scanThread != null) {
+        String msg = "Multiple calls to ScanManager.scan()";
+        logger.error(msg);
+        database.insertError(msg);
+        throw new IllegalStateException(msg);
+      }
+
+      running = true;
+    }
+
+    //Start full set of threads going.
+    for (int i = 0; i <= this.threadPool.getCorePoolSize(); i++) {
+      threadPool.execute(new RetrieveScan(this));
+    }
+
+    while (running) {
+      if (workQueue.isEmpty()) {
+        //Since queue is empty add a retrieving scan.
+        threadPool.execute(new RetrieveScan(this));
+      }
+      try {
+        /*We want to wait a little bit so as to not overload the db with getScan requests.*/
+        Thread.sleep(queryWaitTime);
 				/*Update incase available processors changes.
 				 * This is not a constant, but can vary with the OS according to Oracle Javadoc.
 				 */
-				if(Runtime.getRuntime().availableProcessors() > this.threadPool.getCorePoolSize()){
-					this.threadPool.setCorePoolSize(Runtime.getRuntime().availableProcessors());
-				}
-			} catch (InterruptedException e) {
-				//Shouldn't happen, as nothing on our end signals an Interrupt
-				String msg = Logging.exceptionToString(e);
-				logger.error(msg);
-				database.insertError(msg);
-			}
-		}
+        if (Runtime.getRuntime().availableProcessors() > this.threadPool.getCorePoolSize()) {
+          this.threadPool.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        }
+      } catch (InterruptedException e) {
+        logger.info("Interrupt in ScanManager");
+      }
+    }
 
-	}
+    logger.info("ScanManager halt() called -> Shutting down");
+    scanThread = null;
+  }
 
-	public void addTask(ScanWorker task) {
+  public void addTask(ScanWorker task) {
 		/* We cannot use .submit() because of issues when wrapping the Runnable
 		 * into a FutureTask. Our PriorityQueue requires our task to be
 		 * Comparable while FutureTasks are not.
 		 */
-		threadPool.execute(task);
-	}
+    threadPool.execute(task);
+  }
+
+  /**
+   * Used for testing
+   *
+   * @param time milliseconds
+   */
+  protected void setQueryTime(long time) {
+    queryWaitTime = time;
+  }
+
+  public void halt() {
+    logger.info("ScanManager halting");
+    this.running = false;
+    //Kill the Thread.sleep
+    if (scanThread != null) {
+      scanThread.interrupt();
+    }
+  }
 }
