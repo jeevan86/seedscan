e6337c253a4407fe0dd15ad89c50e7447a82daf0
James Holland <jholland@usgs.gov>
2017-03-15 15:24:30 -0600

    Add GlobalMock and test case for DataLoader

---
+++ src/test/java/asl/seedscan/GlobalMock.java
@@ -0,0 +1,12 @@
+package asl.seedscan;
+
+/**
+ * Created by jholland on 3/15/17.
+ */
+public class GlobalMock extends Global{
+
+  public static void setQualityFlags(String flag){
+    qualityflags = flag;
+  }
+
+}
