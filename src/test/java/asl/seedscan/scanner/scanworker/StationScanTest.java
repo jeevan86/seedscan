31149b16e78780f84ce50fe973a145143154dbe3
James Holland <jholland@usgs.gov>
2017-03-15 17:09:37 -0600

    Add setup for StationScanTest

--- src/test/java/asl/seedscan/scanner/scanworker/StationScanTest.java
+++ src/test/java/asl/seedscan/scanner/scanworker/StationScanTest.java
@@ -2,10 +2,44 @@ package asl.seedscan.scanner.scanworker;
 
 import static org.junit.Assert.*;
 
+import asl.metadata.MetaGenerator;
+import asl.seedscan.database.MetricDatabaseMock;
+import asl.seedscan.scanner.ScanManager;
+import asl.testutils.Dependent;
+import asl.testutils.ResourceManager;
+import java.util.ArrayList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class StationScanTest {
+
+  private static MetaGenerator metaGenerator;
+  private static ScanManager manager;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+
+    Dependent.assumeRDSeed();
+    Dependent.assumeGlobalState();
+    try {
+      metaGenerator = new MetaGenerator(
+          ResourceManager.getDirectoryPath("/dataless"),
+          new ArrayList<>());
+      manager = new ScanManager(new MetricDatabaseMock(), metaGenerator);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    metaGenerator = null;
+    manager.halt();
+    manager = null;
+  }
 
   @Ignore
   @Test
