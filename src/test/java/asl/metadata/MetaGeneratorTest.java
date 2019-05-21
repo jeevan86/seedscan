6abe9ed56a7e29348c4d1d21ed117de70fa31fbe
James Holland <jholland@usgs.gov>
2019-05-21 16:04:10 -0600

    Add new test resources for station dataless

---
+++ src/test/java/asl/metadata/MetaGeneratorTest.java
@@ -0,0 +1,28 @@
+package asl.metadata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MetaGeneratorTest {
+
+  @Test
+  public void buildVolumesFromStringData() {
+  }
+
+  @Test
+  public void addVolume() {
+  }
+
+  @Test
+  public void getStationList() {
+  }
+
+  @Test
+  public void getStationList1() {
+  }
+
+  @Test
+  public void getStationMeta() {
+  }
+}
