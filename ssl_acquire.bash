eaf2e18efdc39516e1e6f6666c7fabdca4fd9bd1
Kearns, Aaron (Contractor) M <akearns@contractor.usgs.gov>
2019-10-29 18:23:58 +0000

    Update ssl_acquire.bash

--- ssl_acquire.bash
+++ ssl_acquire.bash
@@ -5,6 +5,5 @@ wget -O DOIRootCA2.cer http://sslhelp.doi.net/docs/DOIRootCA2.cer
 keytool -import -file DOIRootCA2.cer -alias DOIRootCA2 -keystore cacerts -keypass changeit -storepass changeit -noprompt
 printf 'org.gradle.jvmargs=-Djavax.net.ssl.trustStore=' > gradle.properties
 echo -n `pwd` >> gradle.properties
-printf "/cacerts -Djavax.net.ssl.trustStorePassword=changeit" >> gradle.properties
-printf " -Xms8g -Xmx10g\n" >> gradle.properties
+printf "/cacerts -Djavax.net.ssl.trustStorePassword=changeit\n" >> gradle.properties
 cat gradle.properties
