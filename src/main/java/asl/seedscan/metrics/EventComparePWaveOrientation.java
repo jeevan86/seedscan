ffb3e17be9ce82085f1944d9e5f5624dc40b2eef
Kearns, Aaron (Contractor) M <akearns@contractor.usgs.gov>
2020-04-28 16:01:53 -0600

    Minor tweaks to formatting/comments

--- src/main/java/asl/seedscan/metrics/EventComparePWaveOrientation.java
+++ src/main/java/asl/seedscan/metrics/EventComparePWaveOrientation.java
@@ -119,8 +119,10 @@ public class EventComparePWaveOrientation extends Metric {
       int lastCharIdx = channelVal.length() - 1;
       char last = channelVal.charAt(lastCharIdx);
       if (last == 'Z' || last == 'E') {
-        // assume vertical sensor component requires no orientation
-        // assume east sensor will be referenced when north is read in
+        // we'll assign metric results to ND channels, with the assumption that ED shares the same
+        // orientation, and Z is only useful for phase determination of quadrant
+        // we rescan all channels when doing a whitelist scan (metadata changes), so we can
+        // reasonably presume that the N channel is part of the channel list iterated through
         continue;
       } else if (last == 'D') {
         if (channelVal.charAt(lastCharIdx - 1) == 'E') {
@@ -147,7 +149,7 @@ public class EventComparePWaveOrientation extends Metric {
       double angleDifference = computeMetric(eventCMTs, curChannel, pairChannel, vertChannel);
 
       // Catch any bail out of internal metric.
-      if (Double.isNaN(angleDifference)) {
+      if (angleDifference == NO_RESULT) {
         continue;
       }
 
@@ -306,9 +308,11 @@ public class EventComparePWaveOrientation extends Metric {
 
     //Average our event angle differences.
     if (numEvents <= 0) {
-      return Double.NaN;
+      return NO_RESULT;
     } else {
-      return sumAngleDifference / numEvents;
+      // this is an average, and the returned result should be between +/- 360
+      // which also prevents collision with value NO_RESULT above (-999.999)
+      return (sumAngleDifference / numEvents) % 360;
     }
   }
 
